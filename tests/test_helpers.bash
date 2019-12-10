f9f927424d22fb7f6d276d7b5f593637ed182bea
Fabio Kruger <krufab@gmail.com>
2019-12-10 01:34:49 +0100

    Added alpine flavor

--- tests/test_helpers.bash
+++ tests/test_helpers.bash
@@ -1,5 +1,6 @@
-#!/bin/bash -exu
+#!/usr/bin/env bash
 
+set -eu
 
 # check dependencies
 (
@@ -13,8 +14,8 @@ function assert {
     local expected_output=$1
     shift
     actual_output=$("$@")
-    if ! [ "$actual_output" = "$expected_output" ]; then
-        echo "expected: \"$expected_output\", actual: \"$actual_output\""
+    if ! [[ "$actual_output" = "$expected_output" ]]; then
+        echo "expected: '$expected_output', actual: '$actual_output'"
         false
     fi
 }
@@ -29,17 +30,52 @@ function retry {
 
     for ((i=0; i < attempts; i++)); do
         run "$@"
-        if [ "$status" -eq 0 ]; then
+        if [[ "$status" -eq 0 ]]; then
             return 0
         fi
-        sleep $delay
+        sleep "$delay"
     done
 
-    echo "Command \"$@\" failed $attempts times. Status: $status. Output: $output"
+    echo "Command '$*' failed $attempts times. Status: $status. Output: $output"
     false
 }
 
 # return the published port for given container port $1
 function get_port {
-    docker port $SUT_CONTAINER $1 | cut -d: -f2
+    docker port "${SUT_CONTAINER}" "$1" | cut -d: -f2
+}
+
+# run a given command through ssh on the test container.
+# Use the $status, $output and $lines variables to make assertions
+function run_through_ssh {
+	SSH_PORT=$(get_port 22)
+	if [[ "${SSH_PORT}" = "" ]]; then
+		echo "failed to get SSH port"
+		false
+	else
+		TMP_PRIV_KEY_FILE=$(mktemp "${BATS_TMPDIR}"/bats_private_ssh_key_XXXXXXX)
+		echo "${PRIVATE_SSH_KEY}" > "${TMP_PRIV_KEY_FILE}" \
+		 	&& chmod 0600 "${TMP_PRIV_KEY_FILE}"
+
+		run ssh -i "${TMP_PRIV_KEY_FILE}" \
+			-o LogLevel=quiet \
+			-o UserKnownHostsFile=/dev/null \
+			-o StrictHostKeyChecking=no \
+			-l jenkins \
+			localhost \
+			-p "${SSH_PORT}" \
+			"$@"
+
+		rm -f "${TMP_PRIV_KEY_FILE}"
+	fi
+}
+
+function clean_test_container {
+	docker kill "${SUT_CONTAINER}" &>/dev/null ||:
+	docker rm -fv "${SUT_CONTAINER}" &>/dev/null ||:
+}
+
+function is_slave_container_running {
+	sleep 1  # give time to sshd to eventually fail to initialize
+	retry 3 1 assert "true" docker inspect -f '{{.State.Running}}' "${SUT_CONTAINER}"
 }
